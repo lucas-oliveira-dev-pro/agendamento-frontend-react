2d84d8d4bbbfe9612844219788a9e3ebad5a82fa
Lucas <sergiolucasoliveira01@gmail.com>
2026-09-03 21:56:54 -0300

    Bug: Corrigindo bug em produção

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.API_URL || "http://localhost:3000/api",
+  baseURL: "https://agendamento-api-supabase-sdk.onrender.com/api" || "http://localhost:3000/api",
   headers: { "Content-Type": "application/json" },
 });
 
